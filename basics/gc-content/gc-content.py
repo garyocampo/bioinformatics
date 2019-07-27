8eec10072704affaa8e929cdb08a30a80595c8b2
Gabriel <gabriel@gabriels-MacBook-Pro-6.local>
2019-07-27 13:22:31 -0300

    empty message

--- basics/gc-content/gc-content.py
+++ basics/gc-content/gc-content.py
@@ -46,3 +46,19 @@ parsedFasta = parser(fastaFile)
 countCG = gcCounter(parsedFasta)
 print(getHighest(countCG))
 
+fastaFile = open('./rosalind_hamm.txt').read()
+genes = []
+def read_fasta(fp):
+        name, seq = None, []
+        for line in fp:
+            line = line.rstrip()
+            if line.startswith(">"):
+                if name: yield (name, ''.join(seq))
+                name, seq = line, []
+            else:
+                seq.append(line)
+        if name: yield (name, ''.join(seq))
+
+with open('./rosalind_hamm.txt') as fp:
+    for name, seq in read_fasta(fp):
+        genes.append([name, seq])
